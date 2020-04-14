8a827caed33c0417038a05354b64d4f789107ce2
jpiazza35 <36086658+jpiazza35@users.noreply.github.com>
2020-04-14 08:23:54 -0300

    Create message.js

---
+++ message.js
@@ -0,0 +1,16 @@
+import MessageMixer from './messageMixer';
+
+function displayMessage() {
+  console.log(MessageMixer.countCharacter("What is the color of the sky?", "t"));
+  console.log(MessageMixer.capitalizeFirstCharacterOfWords("What is the color of the sky?"));
+  console.log(MessageMixer.reverseWord("What is the color of the sky?"));
+  console.log(MessageMixer.reverseAllWords("What is the color of the sky?"));
+  console.log(MessageMixer.replaceFirstOccurence("What is the color of the sky?", "sky", "water"));
+  console.log(MessageMixer.encode("What is the color of the sky?"));
+  
+  console.log(MessageMixer.palindrome("River plate"));
+  console.log(MessageMixer.pigLatin("river le rompio el toto as Boca en el 2019?", "y"));
+
+}
+
+displayMessage();
